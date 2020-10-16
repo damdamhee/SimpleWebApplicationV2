e88c696ea70aa7d2d0a3c202ea31998dcc872337
JinHyung Jung <jh9310s@gmail.com>
2020-10-16 20:48:50 +0900

    Revert "[추가] Table 컴포넌트 정의 및 적용"
    This reverts commit 495eec5306f1391eb30b3ca2a34083d24b07d9b4.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,45 +2,25 @@ import Vue from "vue";
 import Header from "@/Header.js";
 import AddPersonBtn from "@/AddPersonBtn.js";
 import Divider from "@/Divider.js";
-import Table from "@/Table.js"
 
 new Vue({
-    components: { 'j-header': Header, 'j-button': AddPersonBtn, 'j-table': Table, 'j-divider': Divider },
-    data(){
-        return {
-            tableHeaders: ["Name", "Phone No.", "Address"],
-            listOfPeople: [
-                { name: 'JJH', phno: '010-1234-5678', addr: 'suwon' },
-                { name: 'JHJ', phno: '010-0987-6543', addr: 'suwon' }
-            ]
-        }
+    components: { 'j-header': Header, 'j-button': AddPersonBtn, 'j-divider': Divider },
+    data() {
+        return {}
     },
-    methods: {
-        addPerson: function () {
-            let newPerson = {name:"", phno:"", addr:""}
-            this.listOfPeople.push(newPerson);
-        }
-    },
+    methods: {},
     render: function (createElement) {
         let headerVDOM = createElement('j-header')
+
         let addPersonBtnVDOM = createElement('j-button', {
             on: {
                 input: this.addPerson
             }
         })
+
         let dividerVDOM = createElement('j-divider');
 
-        let tableAttributes = {
-            props: {
-                tableHeaders : this.tableHeaders,
-                listOfPeople: this.listOfPeople
-            }
-        }
-        let tableVDOM = createElement('j-table', tableAttributes)
-        let ret = createElement('div',
-            {},
-            [headerVDOM, addPersonBtnVDOM, dividerVDOM, tableVDOM]
-        )
+        let ret = createElement('div',{},[headerVDOM, addPersonBtnVDOM, dividerVDOM])
         return ret;
     }
 }).$mount('#main')
