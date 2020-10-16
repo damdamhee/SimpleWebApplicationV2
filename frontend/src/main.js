653ce020f6ddefd43375069a6e4fc79fb5529685
JinHyung Jung <jh9310s@gmail.com>
2020-10-16 20:18:25 +0900

    [추가] Header 컴포넌트 정의 및 적용
    - Header 컴포넌트 정의 (VDOM객체를 export한다)
    - Header 컴포넌트를 루트 컴포넌트(main.js)의 자식 컴포넌트로 등록해서 사용한다

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,16 @@
 import Vue from "vue";
+import Header from "@/Header.js";
 
 new Vue({
     // el:'#main', //이게 없으면 직접 마운트 해주어야 한다
-    components: {},
+    components: { 'j-header': Header },
     data() {
         return {}
     },
     methods: {},
     render: function (createElement) {
-        let ret = createElement('div',{},[])
+        let headerVDOM = createElement('j-header')
+        let ret = createElement('div',{},[headerVDOM])
         return ret;
     }
 }).$mount('#main')
